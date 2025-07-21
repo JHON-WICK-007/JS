608e172b42f7caa35b8808aacb7be0c99899b6b2
MANAN <mananvasani801@gmail.com>
2025-07-21 22:42:15 +0530

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,13 @@
+console.log("Manan");
+console.log(typeof "Manan");
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+const product = {
+    name: "raj",
+    surname: "vasani"
+};
+
+console.log(product);
+console.log(typeof product);
